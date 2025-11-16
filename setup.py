2fc133a7bd1ddd4e96567baa73c37d31cee2aa7d
Jeet-047 <jeet0912majumder@gmail.com>
2025-11-16 21:30:00 +0530

    setup mongodb, and add logging, exception

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Jeet Majumder",
+    author_email="jeet0912majumder@gmail.com",
+    packages=find_packages()
+)
